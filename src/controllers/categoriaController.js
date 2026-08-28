0244e60df6387d84c98d46d64bba5aff60067f7a
cauagabriel <caua.g.silva@edu.senai.br>
2026-08-28 15:15:58 -0300

    feat: Finaliza o esqueleto MVC e audita coerência com o diagrama UML

--- src/controllers/categoriaController.js
+++ src/controllers/categoriaController.js
@@ -1,3 +1,4 @@
 // CONTROLLER: decide o que fazer com pedidos de Categoria.
+// Implementacao chega no Bloco 3.
 
 module.exports = {};
--- src/services/categoriaService.js
+++ src/services/categoriaService.js
@@ -1,3 +1,4 @@
 // SERVICE: executa a lógica de Categoria.
+// Implementacao chega no Bloco 3.
 
 module.exports = {};
